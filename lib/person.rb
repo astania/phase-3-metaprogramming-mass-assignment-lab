d924de653e2b94ca8adb80e754adcf8827a0d412
+ <+>
2022-04-05 17:20:45 -0400

    commit tree 2cd9f9d073210a042a61206f406010951439b41e

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
   # your code here
+
+  def initialize(person_hash)
+
+    person_hash.each do |key, value|
+      
+    self.class.attr_accessor(key)
+    self.send("#{key}=", value)
+    
+    end 
+  end 
 end
